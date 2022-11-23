a88997a9ab112570f75ecd6eb3ca70f0370c6f3d
jahir <rana78085@gmail.com>
2022-11-24 00:14:04 +0600

    Add tailwindcss and daisyui

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2>Hello Resale</h2>
+      <h2 className='btn'>Hello Resale</h2>
+      <button className="btn">Button</button>
+<button className="btn btn-primary">Button</button>
+<button className="btn btn-secondary">Button</button>
+<button className="btn btn-accent">Button</button>
+<button className="btn btn-ghost">Button</button>
+<button className="btn btn-link">Button</button>
     </div>
   );
 }
