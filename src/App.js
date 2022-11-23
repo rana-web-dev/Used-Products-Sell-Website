7323ec287faade674589a4523795028389343098
jahir <rana78085@gmail.com>
2022-11-24 00:31:54 +0600

    Setup Page Routing And Navigation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes/routes/Routes';
 
 function App() {
   return (
     <div className="App">
-      <h2 className='btn'>Hello Resale</h2>
-      <button className="btn">Button</button>
-<button className="btn btn-primary">Button</button>
-<button className="btn btn-secondary">Button</button>
-<button className="btn btn-accent">Button</button>
-<button className="btn btn-ghost">Button</button>
-<button className="btn btn-link">Button</button>
+      <RouterProvider router={router}>
+        
+      </RouterProvider>
     </div>
   );
 }
---
+++ src/layout/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from '../pages/home/nav/Nav';
+
+const Home = () => {
+    return (
+        <div>
+            <Nav></Nav>
+        </div>
+    );
+};
+
+export default Home;
