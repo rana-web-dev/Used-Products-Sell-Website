a687120c9729114b089b1545ac987badab97cf05
jahir <rana78085@gmail.com>
2022-11-25 00:17:44 +0600

    Create acer component to show all acer laptop data

--- src/routes/routes/Routes.js
+++ src/routes/routes/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../../layout/Home";
 import Main from "../../layout/Main";
+import AcerDetails from "../../pages/acerDetails/AcerDetails";
 import BrandCollection from "../../pages/brandCollection/BrandCollection";
 import DellDetails from "../../pages/dellDetails/DellDetails";
 
@@ -24,6 +25,11 @@ const router = createBrowserRouter([
         loader: ({params}) => fetch(`http://localhost:5000/Dell/${params.id}`),
         element: <DellDetails></DellDetails>
       },
+      {
+        path: "/Acer/:id",
+        loader: ({params}) => fetch(`http://localhost:5000/Acer/${params.id}`),
+        element: <AcerDetails></AcerDetails>
+      }
     ],
   },
 ]);
